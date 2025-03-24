c02d269980948fd968190f297ba5ac78a0e9254b
saltchicken <john.eicher89@gmail.com>
2025-03-23 22:03:03 -0700

    Add LiteGraphLoader

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import LiteGraphLoader from './components/LiteGraphLoader'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <LiteGraphLoader />
   </StrictMode>,
 )
