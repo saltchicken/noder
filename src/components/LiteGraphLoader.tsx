c10d781d0c4bffefcfbc81a5cf591a10986163a8
saltchicken <john.eicher89@gmail.com>
2025-03-23 22:02:31 -0700

    LiteGraphLoader to make sure everything is loaded first

---
+++ src/components/LiteGraphLoader.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import App from '../App';
+
+const LiteGraphLoader = () => {
+  const [isLiteGraphLoaded, setIsLiteGraphLoaded] = useState(false);
+
+  useEffect(() => {
+    const checkLiteGraph = () => {
+      if (window.LiteGraph) {
+        setIsLiteGraphLoaded(true);
+        console.log("LiteGraph loaded!")
+      } else {
+        console.log("LiteGraph not loaded yet...")
+        setTimeout(checkLiteGraph, 100);
+      }
+    };
+    
+    checkLiteGraph();
+  }, []);
+
+  if (!isLiteGraphLoaded) {
+    return <div>Loading LiteGraph...</div>;
+  }
+
+  return <App />;
+};
+
+export default LiteGraphLoader;
+
