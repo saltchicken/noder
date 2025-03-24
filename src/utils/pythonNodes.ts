f1f0143305c5f616b495fb5aad42d5c575bc9c99
saltchicken <john.eicher89@gmail.com>
2025-03-23 22:02:50 -0700

    Helper file for registering customNodes

---
+++ src/utils/pythonNodes.ts
@@ -0,0 +1,36 @@
+export async function registerCustomNodes(LiteGraph: any) {
+  // Get custom nodes from server
+  const response = await fetch('http://10.0.0.7:8001/custom_nodes', {
+    method: "POST"
+  });
+
+  const result = await response.json();
+  if (result['status'] === 'success') {
+    for (let node of result['nodes']) {
+      registerNode(LiteGraph, node);
+    }
+  }
+}
+
+function registerNode(LiteGraph: any, node: any) {
+  function customNode() {
+    this.title = node.title || "Node";
+    this.uniqueID = crypto.randomUUID();
+    
+    // Add inputs
+    if (node.inputs) {
+      node.inputs.forEach((input: any, i: number) => {
+        this.addInput(input.name, input.type);
+      });
+    }
+
+    // Add outputs
+    if (node.outputs) {
+      node.outputs.forEach((output: any, i: number) => {
+        this.addOutput(output.name, output.type);
+      });
+    }
+  }
+
+  LiteGraph.registerNodeType(node['name'], customNode);
+}
