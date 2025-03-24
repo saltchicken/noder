363cb9d4f5e5fa045fc88cd2efe6ea3841b322b9
saltchicken <john.eicher89@gmail.com>
2025-03-23 22:03:27 -0700

    Check to make sure not initializing LiteGraph twice

--- src/components/LiteGraph.tsx
+++ src/components/LiteGraph.tsx
@@ -3,10 +3,11 @@ import React, { useEffect, useRef, useCallback } from "react";
 const LiteGraphComponent = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const graphRef = useRef(null);
+  const isInitialized = useRef(false);
 
   const handleGraphChange = useCallback((graph) => {
     // Send updated graph state to backend
-    fetch("http://10.0.0.7:8000/process", {
+    fetch("http://10.0.0.7:8001/process", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ 
@@ -16,7 +17,8 @@ const LiteGraphComponent = () => {
   }, []);
 
   useEffect(() => {
-    if (window.LiteGraph && canvasRef.current) {
+    if (window.LiteGraph && canvasRef.current && !isInitialized.current) {
+      console.log("Doing the things");
       const graph = new window.LiteGraph.LGraph();
       graphRef.current = graph;
 
@@ -25,27 +27,28 @@ const LiteGraphComponent = () => {
       // graph.onNodeRemoved = () => handleGraphChange(graph);
       // graph.onConnectionChange = () => handleGraphChange(graph);
       const canvas = new window.LiteGraph.LGraphCanvas(canvasRef.current, graph);
+      // registerCustomNodes(window.LiteGraph);
       // graph.start();
       canvas.resize();
+      isInitialized.current = true;
+
    }
   }, [handleGraphChange]);
 
   // Add methods to interact with the graph
-  const addNewNode = useCallback((type: string, pos: [number, number]) => {
-    if (graphRef.current) {
-      const node = LiteGraph.createNode(type);
-      node.pos = pos;
-      graphRef.current.add(node);
-    }
-  }, []);
+  // const addNewNode = useCallback((type: string, pos: [number, number]) => {
+  //   if (graphRef.current) {
+  //     const node = LiteGraph.createNode(type);
+  //     node.pos = pos;
+  //     graphRef.current.add(node);
+  //   }
+  // }, []);
 
   return (
-    <div>
       <canvas
         ref={canvasRef}
         style={{ width: "100%", height: "100%" }}
       ></canvas>
-    </div>
   );
 };
 
