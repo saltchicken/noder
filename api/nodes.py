7da3ccab4796210937c1a8296eb253984daded74
saltchicken <john.eicher89@gmail.com>
2025-03-26 18:59:39 -0700

    Add OllamaQuery node

--- api/nodes.py
+++ api/nodes.py
@@ -37,3 +37,28 @@ class Bar(Node):
         print(f"{BarOutput} and {BarOutput2}")
 
         return BarOutput, BarOutput2
+
+class OllamaQuery(Node):
+    def run(self, model: str, system_message: str, prompt: str, host: str, port: str, temperature: str, seed: str) -> Tuple[str, str]:
+        from ollama_query import ollama_query
+        model_text = self.widget_values[0]
+        system_message_text = self.widget_values[1]
+        prompt_text = self.widget_values[2]
+        host_text = self.widget_values[3]
+        port_text = self.widget_values[4]
+        temperature_text = self.widget_values[5]
+        seed_text = self.widget_values[6]
+
+        if system_message_text == "":
+            system_message_text = None
+
+        if temperature_text == "":
+            temperature_text = None
+
+        if seed_text == "":
+            print("SETTING SEED TO NONE")
+            seed_text = None
+
+        response, debug_text = ollama_query(model=model_text, prompt=prompt_text, system_message=system_message_text, host=host_text, port=port_text, temperature=temperature_text)
+
+        return (response, debug_text)
