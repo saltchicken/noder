4ddc93b0209f0850487f1ab585179d4ff16d0382
saltchicken <john.eicher89@gmail.com>
2025-04-01 19:34:32 -0700

    Change theme of edges

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,10 @@ import '@xyflow/react/dist/style.css';
 import ContextMenu from './components/ContextMenu';
 
 import CustomNode from './nodes/CustomNode.tsx';
-import CustomEdge from './edges/CustomEdge.jsx';
 
 const nodeTypes = {
   customNode: CustomNode
 };
-
-const edgeTypes = {
-  custom: CustomEdge
-}
 
 
 const Flow = () => {
@@ -207,9 +202,6 @@ const Flow = () => {
         onMove={onPaneMove}
         onNodeContextMenu={onNodeContextMenu}
         nodeTypes={nodeTypes}
-        edgeTypes={edgeTypes}
-        defaultEdgeOptions={{ type: 'custom' }}
-        connectionLineStyle={{ stroke: '#33FF71' , strokeWidth: 3 }}
         fitView
         colorMode="dark"
       >
