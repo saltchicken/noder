ab1f13ff705c2d0b00936657c6a448db7fbeafab
saltchicken <john.eicher89@gmail.com>
2025-04-01 18:05:43 -0700

    Implement resize for nodes

--- src/nodes/TextUpdaterNode.tsx
+++ src/nodes/TextUpdaterNode.tsx
@@ -1,7 +1,13 @@
+import { memo } from 'react';
 import { useCallback } from 'react';
-import { Handle, Position } from '@xyflow/react';
+import { Handle, Position , NodeResizeControl} from '@xyflow/react';
 
 import SingleConnectionHandle from '../components/SingleConnectionHandle.jsx'
+
+const controlStyle = {
+  background: 'transparent',
+  border: 'none',
+};
 
 function TextUpdaterNode({ data }) {
   const onChange = useCallback((evt) => {
@@ -15,6 +21,9 @@ function TextUpdaterNode({ data }) {
 
   return (
     <>
+      <NodeResizeControl style={controlStyle} minWidth={100} minHeight={50}>
+        <ResizeIcon />
+      </NodeResizeControl>
       {inputs.map((inputId, index) => (
         <div key={inputId}>
           <SingleConnectionHandle 
@@ -63,5 +72,28 @@ function TextUpdaterNode({ data }) {
   );
 }
 
-export default TextUpdaterNode;
+function ResizeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="#ff0071"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      style={{ position: 'absolute', right: 5, bottom: 5 }}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <polyline points="16 20 20 20 20 16" />
+      <line x1="14" y1="14" x2="20" y2="20" />
+      <polyline points="8 4 4 4 4 8" />
+      <line x1="4" y1="4" x2="10" y2="10" />
+    </svg>
+  );
+}
+
+export default memo(TextUpdaterNode);
 
