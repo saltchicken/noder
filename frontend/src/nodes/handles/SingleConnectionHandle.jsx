fda22737d12e8ef55215af7f1fb3038e450a92dc
saltchicken <john.eicher89@gmail.com>
2025-04-05 19:49:44 -0700

    Revert SingleConnectionHandle

--- frontend/src/nodes/handles/SingleConnectionHandle.jsx
+++ frontend/src/nodes/handles/SingleConnectionHandle.jsx
@@ -6,28 +6,11 @@ const SingleConnectionHandle = (props) => {
     handleType: props.type,
     handleId: props.id
   });
-
-  const isTypeCompatible = (sourceType, targetType) => {
-    return sourceType === targetType;
-  };
  
   return (
     <Handle
       {...props}
-      isConnectable={(connection) => {
-        if (connections.length >= 1) return false;
-        if (props.type === 'target' && connection.source) {
-          const sourceHandle = document.querySelector(
-            `[data-nodeid="${connection.source}"] [data-handleid="${connection.sourceHandle}"]`
-          );
-          const sourceType = sourceHandle?.getAttribute('data-type');
-          const targetType = props['data-type'];
-          
-          return isTypeCompatible(sourceType, targetType);
-        }
-        
-        return true;
-      }}
+      isConnectable={connections.length < 1}
     />
   );
 };
