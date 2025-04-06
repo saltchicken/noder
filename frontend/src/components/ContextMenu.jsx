f46c0abe84742a4662ebd1afacf4058bceb7ef9b
saltchicken <john.eicher89@gmail.com>
2025-04-06 15:14:51 -0700

    uuidv4 moved to utils

--- frontend/src/components/ContextMenu.jsx
+++ frontend/src/components/ContextMenu.jsx
@@ -1,13 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { useReactFlow } from '@xyflow/react';
-
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
+import { uuidv4 } from '../utils/uuid';
 
 
 export default function ContextMenu({
