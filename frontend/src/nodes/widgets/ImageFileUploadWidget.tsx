4e74c5e556a9abb64f87815d7063915a157ff10b
saltchicken <john.eicher89@gmail.com>
2025-04-06 17:35:02 -0700

    Put image on top of "Choose File" dialog

--- frontend/src/nodes/widgets/ImageFileUploadWidget.tsx
+++ frontend/src/nodes/widgets/ImageFileUploadWidget.tsx
@@ -19,36 +19,6 @@ const ImageFileUploadWidget = ({ widget, onChange }) => {
 
   return (
     <div style={{ padding: '0px 0px 10px 0px', position: 'relative' }}>
-      <span style={{
-        position: 'absolute',
-        left: '20px',
-        top: '5px',
-        fontSize: '6px',
-        color: '#AAA',
-        zIndex: 1,
-        pointerEvents: 'none'
-      }}>
-        {widget.name}
-      </span>
-      <input
-        type="file"
-        accept="image/*"
-        id={widget.name}
-        name={widget.name}
-        onChange={handleFileChange}
-        className="nodrag"
-        style={{
-          width: 'calc(100% - 40px)',
-          padding: '8px',
-          border: '1px solid #333',
-          borderRadius: '5px',
-          fontSize: '12px',
-          backgroundColor: '#1e1e1e',
-          color: '#fff',
-          paddingLeft: '7px',
-          paddingTop: '12px'
-        }}
-      />
       {imageUrl && (
         <div style={{ marginTop: '10px' }}>
           <img
@@ -68,6 +38,25 @@ const ImageFileUploadWidget = ({ widget, onChange }) => {
           />
         </div>
       )}
+      <input
+        type="file"
+        accept="image/*"
+        id={widget.name}
+        name={widget.name}
+        onChange={handleFileChange}
+        className="nodrag"
+        style={{
+          width: 'calc(100% - 40px)',
+          padding: '8px',
+          border: '1px solid #333',
+          borderRadius: '5px',
+          fontSize: '12px',
+          backgroundColor: '#1e1e1e',
+          color: '#fff',
+          paddingLeft: '7px',
+          paddingTop: '12px'
+        }}
+      />
     </div>
   );
 };
